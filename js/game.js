41650c6d72d03fe0031aebea05fb9bc025f4be1f
Jeffrey <jeffreyvdk@live.nl>
2020-09-08 16:31:26 +0200

    Add random computer play to the game

--- js/game.js
+++ js/game.js
@@ -1,1 +1,18 @@
-console.log("test");
+// computer play
+function computerPlay(max) {
+  let randomNumber = Math.floor(Math.random() * Math.floor(max));
+  if (randomNumber === 0) {
+    computerChoice = "Rock";
+  } else if (randomNumber === 1) {
+    computerChoice = "Paper";
+  } else {
+    computerChoice = "Scissors";
+  }
+  return;
+}
+
+
+
+// call functions
+computerPlay(3);
+console.log(`computer choice: ${computerChoice}`);

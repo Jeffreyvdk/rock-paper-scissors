e3a73e3bbe509b583976abb7250805bb881b4904
Jeffrey <jeffreyvdk@live.nl>
2020-09-15 19:30:51 +0200

    Add function to play 5 rounds and to keep score

--- js/game.js
+++ js/game.js
@@ -1,26 +1,49 @@
-// computer play
+// function to return a random computerchoice.
 function computerPlay() {
   let randomNumber = Math.floor(Math.random() * Math.floor(3));
   if (randomNumber === 0) {
-    return 'Rock';
+    return "rock";
   } else if (randomNumber === 1) {
-    return 'Paper';
+    return "paper";
   } else {
-    return 'Scissors';
+    return "scissors";
   }
 }
 
-// play one round
-function playRound(playerSelection, computerSelection) {
-  if (playerSelection === 'Rock' && computerSelection === 'Rock') {
-    return 'It\'s a draw';
-  } else if (playerSelection === 'Rock' && computerSelection === 'Paper') {
-    return 'Computer wins';
-  } else { 
-    return 'Player wins';
-  }
- }
+// function to play 5 rounds.
+function game() {
+  for (let i = 0; i < 5; i++) {
+    // function to play one round.
+    function playRound(playerSelection, computerSelection) {
+      if (playerSelection === "rock" && computerSelection === "rock") {
+        console.log("It's a draw");
+        return (roundScore = 0);
+      } else if (playerSelection === "rock" && computerSelection === "paper") {
+        console.log("You Lose! Paper beats Rock");
+        return (roundScore = 1);
+      } else {
+        console.log("You Win! Rock beats Scissors");
+        return (roundScore = 2);
+      }
+    }
+    const playerSelection = "RoCk".toLowerCase();
+    const computerSelection = computerPlay();
+    console.log(playRound(playerSelection, computerSelection));
 
-const playerSelection = 'Rock'
-const computerSelection = computerPlay()
-console.log(playRound(playerSelection, computerSelection))
+    // keep score
+    if (roundScore === 1) {
+      computerScore++;
+    } else if (roundScore === 2) {
+      playerScore++;
+    }
+  }
+}
+
+// create variables to keep the score
+let roundScore;
+let computerScore = 0;
+let playerScore = 0;
+
+// call function to play the game
+game();
+console.log(`computerscore: ${computerScore} / playerscore ${playerScore}`);
